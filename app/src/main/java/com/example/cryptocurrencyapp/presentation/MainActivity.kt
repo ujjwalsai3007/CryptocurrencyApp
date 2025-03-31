092a46b864bd783813bde8baa164f344617d17e0
Ujjwal sai <ujjwalsai501@gmail.com>
2025-03-31 11:10:37 +0530

    Added detailScreen

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
@@ -4,14 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrencyapp.presentation.coin_detail.CoinListScreen
 import com.example.cryptocurrencyapp.presentation.ui.theme.CryptoCurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CryptoCurrencyAppTheme {
+                val navController= rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screens.CoinListScreen.route
+                ){
+                    composable(
+                        route=Screens.CoinListScreen.route
+                    ) {
+                        CoinListScreen(navController)
+                    }
+                    composable(
+                        route=Screens.CoinDetailScreen.route
+                    ) {
+                        CoinDetailScreen()
+                    }
+                }
+
 
             }
         }
